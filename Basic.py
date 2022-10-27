b726abea3750007b9554d79c2027af8b984e077e
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2022-10-27 21:34:19 +0600

    face comparison false tested

--- Basic.py
+++ Basic.py
@@ -3,8 +3,8 @@ import face_recognition
 
 # call image
 Elon_Musk = cv2.imread("resources/Elon_Musk.jpg")
+Bill_Gates = cv2.imread("resources/Jeff_Bezos.jpg")
 # test_Elon_Musk= cv2.imread("resources/test_Elon_Musk.jpg")
-Bill_Gates = cv2.imread("resources/Bill_Gates.jpg")
 
 # identify the face from thr image
 #(top,right,bottom,left)
@@ -20,8 +20,8 @@ cv2.rectangle(Bill_Gates,(bill_face_location[3], bill_face_location[0]),(bill_fa
 
 # comparison between 2 images
 encodedElon = face_recognition.face_encodings(Elon_Musk)[0]
-# encodedTest = face_recognition.face_encodings(test_Elon_Musk)[0]
 encodedBill = face_recognition.face_encodings(Bill_Gates)[0]
+# encodedTest = face_recognition.face_encodings(test_Elon_Musk)[0]
 # print(encodedBill)
 
 # comparison_result = face_recognition.compare_faces([encodedElon],encodedTest)
@@ -34,8 +34,8 @@ print(comparison_result)
 
 # display images
 cv2.imshow("Elon Musk", Elon_Musk)
-# cv2.imshow("test_Elon_Musk",test_Elon_Musk)
 cv2.imshow("Bill Gates",Bill_Gates)
+# cv2.imshow("test_Elon_Musk",test_Elon_Musk)
 
 # the image will wait after display
 cv2.waitKey(10000)
