f125ed78584dcd3e322ca478c7d89fa02273fefa
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2022-11-01 00:33:07 +0600

    react-native expo android camera app

--- AttendanceApp/App.js
+++ AttendanceApp/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
 import { useEffect, useRef, useState } from 'react';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
@@ -11,39 +11,102 @@ export default function App() {
 	// setting the camera and media access permissions 
 	const [hasCameraPermission, setHasCameraPermission] = useState();
 	const [hasMediaPermission, setHasMediaPermission] = useState();
+	
+	// whether a photo has been clicked or not 
+	const [photo, setPhoto] = useState();
 
 	useEffect(() => {
 		(async() => {
 			const CameraPermission = await Camera.requestCameraPermissionsAsync();
 			const MediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
 			setHasCameraPermission(CameraPermission.status === "granted");
-			setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+			setHasMediaPermission(MediaLibraryPermission.status === "granted");
 		})();
 	}, []);
 
-	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+	// text to display when camera permission not given 
+	if (hasCameraPermission === undefined) {
+		return <Text>Requesting permissions... </Text>
+	}
+	else if (!hasCameraPermission) {
+		return <Text>Permission for Camera not granted</Text>
+	}
 
-// text to display when camera permission not given 
-if(hasCameraPermission === undefined)
-{
-	return <Text>Requesting permissions... </Text>
-}
-else if(!hasCameraPermission)
-{
-	return <Text>Permission for Camera not granted</Text>
+	// taking pictures would be a asynchronous process
+	let takePic = async() =>
+	{
+		let options =
+		{
+			quality: 1,
+			base64: true,
+			exif: false
+		};
+
+		let newPhoto = await cameraRef.current.takePictureAsync(options);
+		setPhoto(newPhoto);
+	}
+
+	// if a new photo has been clicked 
+	if(photo)
+	{
+		// share the photo 
+		let sharePic = () =>
+		{
+			shareAsync(photo.uri).then(() => {
+				setPhoto(undefined);
+			});
+		};
+
+		// save a photo to the gallery 
+		let savePhoto = () =>
+		{
+			MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+				setPhoto(undefined);
+			});		
+		};
+
+		return
+		{
+			// so that the screen is well aligned 
+			<SafeAreaView style={styles.container}>
+				<Image style={styles.preview} source={{ uri: "data:image/jpg;base64" + photo.base64 }}/>
+				<Button title='Share' onPress={sharePic}/> 
+
+				{/* can only save a photo if media permission is given */}
+				{hasMediaPermission ? <Button title="Save" onPress={savePhoto} />: undefined}
+				
+				{/* the button to discard the image  */}
+				<Button title='Discard' onPress={() => setPhoto(undefined)}/>
+			</SafeAreaView>
+		};
+	}
+
+	return (
+		// passing a reference to the camera and taking a pic upon clicking 
+		<Camera style={styles.container} ref={cameraRef}>
+		<View style={styles.buttonContainer}>
+			<Button title='Click Picture' onPress={takePic}/>
+		</View>
+			<StatusBar style="auto" />
+		</Camera>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',
+		justifyContent: 'center'
 	},
+	// styling for the camera button 
+	buttonContainer:
+	{
+		backgroundColor: '#fff',
+		alignSelf: 'flex-end'
+	},
+	preview:
+	{
+		alignSelf: 'stretch',
+		flex: 1
+	}
 });
