d41ce9420c5f357d83bcaf334ee078ceafd79603
Zia Uddin Chowdhury <rifatxia9@gmail.com>
2022-10-31 23:03:08 +0600

    react-native expo camera permissions handled

--- AttendanceApp/App.js
+++ AttendanceApp/App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Camera } from 'expo-camera';
+import { shareAsync } from 'expo-sharing';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+	let cameraRef = useRef();
+	// setting the camera and media access permissions 
+	const [hasCameraPermission, setHasCameraPermission] = useState();
+	const [hasMediaPermission, setHasMediaPermission] = useState();
+
+	useEffect(() => {
+		(async() => {
+			const CameraPermission = await Camera.requestCameraPermissionsAsync();
+			const MediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+			setHasCameraPermission(CameraPermission.status === "granted");
+			setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+		})();
+	}, []);
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+// text to display when camera permission not given 
+if(hasCameraPermission === undefined)
+{
+	return <Text>Requesting permissions... </Text>
+}
+else if(!hasCameraPermission)
+{
+	return <Text>Permission for Camera not granted</Text>
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
