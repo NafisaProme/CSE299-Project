5cd722b670b63aa9e8523166764360c2d0c356ff
Nafisa Prome <aymannafisapromi390@gmail.com>
2022-10-31 19:49:05 +0600

    detect page and show name

--- attendance_sys.py
+++ attendance_sys.py
@@ -1,7 +1,34 @@
 from sre_constants import SUCCESS
 import cv2
 import face_recognition
+import os
+import numpy as np
 
+# get the image list
+PATH = "resources"
+imgs = []
+image_files = os.listdir(PATH)
+images = []
+# get the name of individual peoples
+persons_names = []
+
+# open all the image list
+for img_file in image_files:
+    img = cv2.imread(f"{PATH}/{img_file}")
+    images.append(img)
+    person_name = img_file.split(".")[0]
+    persons_names.append(person_name)
+
+# a function that will encode all of the images
+def faceEncodings(img_list):
+    encoding_lst = []
+
+    for image in img_list:
+        encoding = face_recognition.face_encodings(image)[0]
+        encoding_lst.append(encoding)
+    return encoding_lst
+
+known_encodings_lst = faceEncodings(images)
 # capture video
 cap = cv2.VideoCapture(0)
 
@@ -10,6 +37,24 @@ while True:
 
     if not SUCCESS:
         break
+
+    test_faces_location = face_recognition.face_locations(img)
+    test_encoded = face_recognition.face_encodings(img, test_faces_location)
+
+    for encoded_face, location in zip(test_encoded, test_faces_location):
+        matches = face_recognition.compare_faces(known_encodings_lst , test_encoded[0])
+        face_distances = face_recognition.face_distance(known_encodings_lst,test_encoded[0])
+        # print(face_distances)
+        match_index = np.argmin(face_distances)
+
+        if matches[match_index]:
+            name = persons_names[match_index]
+
+            # loose the location
+            y1,x2,y2,x1 = location
+            cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,0),3)
+            # show name beside img box
+            cv2.putText(img,name,(x1+8,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
