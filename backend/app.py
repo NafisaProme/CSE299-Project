763bc6d65d6a13356647b5e27a51fa3a6d9280b3
Nafisa Prome <aymannafisapromi390@gmail.com>
2022-12-05 12:59:00 +0600

    python script auto upload

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,19 @@
 import json
 import requests
-from datetime import date
+from datetime import datetime
 
-headers = {"Authorization":"Bearer ya29.a0AeTM1icZmKHTSlssq1y9IuWDU1Qxi4ELjpdYisgDxB34FE7wxwPcvMGKFERUcXwgBirYzTxo-4jFXHrjyjtTRBekQWyBXvt6HtziQl2hGQUqfuYUwL_y9jL6hTRASN0JX0fYX9Ibpwx_e8mlfhPHvi6tjvMHaCgYKAe0SARESFQHWtWOmI4eriWMu7URGExZMaMEM_Q0163"}
-para = {
-    "name": str(date.today().strftime("%d/%m/%Y")),
-    "parents":["1yJiFzXpGRCYKsQZp3K8BS4LKPWnHvSOh"]
-}
-files = {
-    'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),'file':open("backend/attendance.csv","rb")
-}
-r = requests.post(
-    "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
-    headers=headers,
-    files=files
-)
-print(r.text)
+def work():
+    headers = {"Authorization":"Bearer ya29.a0AeTM1iepKly0giUY37o2XYmvHTnRDh1342lHx844-Abwp03nEAQ2CMgnXcUiRPes9J1GAK-i8tlLuvUv5cJwxteyEOp0EbK5Yz79Ec7kS7HTAAWNzsQiJFt4hINDz1Fad0ka7o_5UrWZDOcBK_x_sS7Ez5LBaCgYKAQ0SARESFQHWtWOm6aUkyb3R9z5E0B-BFeoEgg0163"}
+    para = {
+        "name": str(datetime.today().strftime("%d/%m/%Y")) + " " + str(datetime.now().strftime("%H:%M:%S")),
+        "parents":["1yJiFzXpGRCYKsQZp3K8BS4LKPWnHvSOh"]
+    }
+    files = {
+        'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'),'file':open("backend/attendance.csv","rb")
+    }
+    r = requests.post(
+        "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
+        headers=headers,
+        files=files
+    )
+    print(r.text)
