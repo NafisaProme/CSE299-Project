705e15548542d8a7544476fd5eff73cc197ad25b
Nafisa Prome <aymannafisapromi390@gmail.com>
2022-12-09 18:43:55 +0600

    auto uploading python script to google drive after certain time

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 
 def work():
-    headers = {"Authorization":"Bearer ya29.a0AeTM1iepKly0giUY37o2XYmvHTnRDh1342lHx844-Abwp03nEAQ2CMgnXcUiRPes9J1GAK-i8tlLuvUv5cJwxteyEOp0EbK5Yz79Ec7kS7HTAAWNzsQiJFt4hINDz1Fad0ka7o_5UrWZDOcBK_x_sS7Ez5LBaCgYKAQ0SARESFQHWtWOm6aUkyb3R9z5E0B-BFeoEgg0163"}
+    headers = {"Authorization":"Bearer ya29.a0AeTM1idzzs03nak2EopUq15cJgyTaJd0nzKnE4Nj27aQnEIFuodCDeqUjMsCuPz3g-KT_IOzTrqPvSyFLW21-Gd8ahrMMekxZfIehYcJbIu3rjU-3sCeWaWFpqCg0ypKemYpsmT1snN8rBl7htB-k5iRtvtraCgYKAbISARESFQHWtWOm2qasiaPnarDFWR6rO61avw0163"}
     para = {
         "name": str(datetime.today().strftime("%d/%m/%Y")) + " " + str(datetime.now().strftime("%H:%M:%S")),
         "parents":["1yJiFzXpGRCYKsQZp3K8BS4LKPWnHvSOh"]
