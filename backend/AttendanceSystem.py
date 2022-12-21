9dc9865366b9393cb14c94566de330e6fbc9487b
Nafisa Prome <aymannafisapromi390@gmail.com>
2022-12-21 23:07:05 +0600

    comments updated

--- backend/AttendanceSystem.py
+++ backend/AttendanceSystem.py
@@ -4,11 +4,13 @@ from DriveUploader import *
 from FirebaseRetriever import *
 from MarkAttendance import *
 
+#calling the function
 def run_app():
     import_image()
     attendance()
     upload_to_drive()
 
+#scheduling the function to run automatically
 schedule.every(10).seconds.do(run_app)
 
 while 1:
--- backend/DriveUploader.py
+++ backend/DriveUploader.py
@@ -2,6 +2,7 @@ import json
 import requests
 from datetime import datetime
 
+#drive api authorization
 def upload_to_drive():
     headers = {"Authorization": "Bearer ya29.a0AX9GBdWX7VoODy5l9gs_4sFIjQQRLJ1Ymg0gDyu7M7MhHAynwkhOB7Q6sQzqlRlPceg28EHJ3VEIvWiD04H7-bdWkE2pGuAJ7moutP8bmok2HuMNn3wX2cya9SEv6Uue7kzLs0kIXeyhVibXMONZzQ43Kl-0aCgYKARUSARMSFQHUCsbCHtbqy3ihfb-yR5bwvmsetA0163"}
     para = {
@@ -11,6 +12,7 @@ def upload_to_drive():
     files = {
         'data': ('metadata', json.dumps(para), 'application/json; charset=UTF-8'), 'file': open("backend\\attendance.csv", "rb")
     }
+    #post request
     r = requests.post(
         "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart",
         headers=headers,
